454a41f7565bc5c7070e5af1ec27ce72ba2d190a
Thanik Bharani <thanik.bharani2020@gmail.com>
2026-04-10 02:01:19 -0700

    Update mortgage-calculator.cpp

--- mortgage-calculator.cpp
+++ mortgage-calculator.cpp
@@ -1,6 +1,9 @@
 /*
    Name: Thanik Bharani
    Lab #: 5
+   Mortgage Calculator
+   Calculates monthly mortgage payments and total housing cost
+   using interest rate, loan duration, taxes, utilities, and insurance.
 */
 
 #include <iostream>
